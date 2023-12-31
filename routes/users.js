53c8e44bdb39b27a182b858eeb60f4b762d9965e
ItsAnkitPatel <itsakppatel@gmail.com>
2023-12-31 08:29:54 +0530

    Schema created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
+const plm = require('passport-local-mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userSchema = mongoose.Schema({
+  fullname: String,
+  username: String,
+  email: String,
+  contact: Number,
+  password: String,
+  boards: {
+    type: Array,
+    default: [],
+  },
 });
-
-module.exports = router;
+userSchema.plugin(plm)
+module.exports = mongoose.model("users", userSchema);
